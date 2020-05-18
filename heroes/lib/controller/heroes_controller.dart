48487f53cf45db724c73ed90b6bb7f20d000e660
Wanningjing <13655519148@163.com>
2020-05-18 14:42:54 +0800

    初始框架

--- heroes/lib/controller/heroes_controller.dart
+++ heroes/lib/controller/heroes_controller.dart
@@ -1,35 +1,29 @@
-import 'package:aqueduct/aqueduct.dart';
 import 'package:heroes/heroes.dart';
 import 'package:heroes/model/hero.dart';
-
-class HeroesController extends Controller {
-  
-  final _heroes = [
-    {'id': 11, 'name': 'Mr. Nice'},
-    {'id': 12, 'name': 'Narco'},
-    {'id': 13, 'name': 'Bombasto'},
-    {'id': 14, 'name': 'Celeritas'},
-    {'id': 15, 'name': 'Magneta'},    
-  ];
-
-  @override
-  Future<RequestOrResponse> handle(Request request) async {
-    return Response.ok(_heroes);
-  }
-}
 
 class HeroesController extends ResourceController {
   HeroesController(this.context);
 
   final ManagedContext context;
 
- @Operation.get()
-Future<Response> getAllHeroes({@Bind.query('name') String name}) async {
-  final heroQuery = Query<Hero>(context);
-  if (name != null) {
-    heroQuery.where((h) => h.name).contains(name, caseSensitive: false);
-  }
-  final heroes = await heroQuery.fetch();
+  @Operation.get()
+  Future<Response> getAllHeroes() async {
+    final heroQuery = Query<Hero>(context);
+    final heroes = await heroQuery.fetch();
 
-  return Response.ok(heroes);
+    return Response.ok(heroes);
+  }
+
+@Operation.get('id')
+Future<Response> getHeroByID(@Bind.path('id') int id) async {
+  final heroQuery = Query<Hero>(context)
+    ..where((h) => h.id).equalTo(id);    
+
+  final hero = await heroQuery.fetchOne();
+
+  if (hero == null) {
+    return Response.notFound();
+  }
+  return Response.ok(hero);
+}
 }
