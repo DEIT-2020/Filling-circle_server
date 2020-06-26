59ce3e0293f4c7a6ce4db8f857d046fabfcd2684
Wanningjing <13655519148@163.com>
2020-06-26 22:11:14 +0800

    server

--- heroes/lib/model/pass.dart
+++
@@ -1,1 +0,0 @@
-import "package:heroes/heroes.dart";
---
+++ heroes/migrations/content.migration.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:aqueduct/aqueduct.dart';   
+
+class Migration1 extends Migration { 
+  @override
+  Future upgrade() async {
+   		database.createTable(SchemaTable("_Content",
+        [SchemaColumn("contentId", ManagedPropertyType.bigInteger,
+         isPrimaryKey: true, 
+         autoincrement: true, 
+         isIndexed: false, 
+         isNullable: false, 
+         isUnique: false),
+
+         SchemaColumn("userid", ManagedPropertyType.string, 
+         isPrimaryKey: ture, 
+         autoincrement: false, 
+         isIndexed: false, 
+         isNullable: false, 
+         isUnique: false),
+
+        SchemaColumn("content", ManagedPropertyType.string, 
+        isPrimaryKey: false,
+        autoincrement: false, 
+        isIndexed: false, 
+        isNullable: false, 
+        isUnique: false)]));
+
+       database.addColumn("_Content", 
+       SchemaColumn.relationship("checkby", ManagedPropertyType.bigInteger, 
+       relatedTableName: 
+       "_userid", relatedColumnName: "userid", 
+       rule: DeleteRule.cascade, 
+       isNullable: true, 
+       isUnique: false));
+  }
+  
+  @override
+  Future downgrade() async {}
+  
+  @override
+  Future seed() async {}
+}
+    
