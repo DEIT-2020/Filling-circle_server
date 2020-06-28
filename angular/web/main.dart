bf2065e80cd96ffb205ca0bbfe5fcad551e9235a
jinhao <howardking74@foxmail.com>
2020-06-28 21:51:13 +0800

    html

--- angular/web/main.dart
+++ angular/web/main.dart
@@ -1,6 +1,13 @@
 import 'package:angular/angular.dart';
 import 'package:angular_dart/app_component.template.dart' as ng;
 
+
 void main() {
-  runApp(ng.AppComponentNgFactory);}
-  
+  runApp(ng.AppComponentNgFactory);
+}
+
+
+
+
+
+
