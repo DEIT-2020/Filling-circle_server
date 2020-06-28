8c6611a2558837d0a1b01f4299ebd4a51362a101
Wanningjing <13655519148@163.com>
2020-06-28 21:29:51 +0800

    main.dart

--- angular/web/main.dart
+++ angular/web/main.dart
@@ -1,6 +1,20 @@
+import 'dart:html';
+
 import 'package:angular/angular.dart';
 import 'package:angular_dart/app_component.template.dart' as ng;
 
+InputElement inputcontent;
+OutputElement outputcontent;
+
 void main() {
   runApp(ng.AppComponentNgFactory);}
+
+void getstring(){
+  inputcontent = querySelector('#inputcontent');
+  outputcontent=querySelector('#outputcontent');
+}
+
+void transferpp(){
+  
+}
   
