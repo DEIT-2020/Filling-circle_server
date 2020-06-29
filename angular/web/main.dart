c40cd3fc528e3d58f67bbb3a0217f513872c0907
jinhao <howardking74@foxmail.com>
2020-06-29 08:58:16 +0800

    html4

--- angular/web/main.dart
+++ angular/web/main.dart
@@ -1,38 +1,39 @@
 import 'dart:html';
 
 import 'package:angular/angular.dart';
+
 import 'package:angular_dart/app_component.template.dart' as ng;
 
-// InputElement inputcontent;
-// InputElement submit;
-// OutputElement outputcontent;
-
-// void main() {
-//   runApp(ng.AppComponentNgFactory);}
-
-// void getstring(){
-//   inputcontent = querySelector('#inputcontent');
-//   outputcontent=querySelector('#outputcontent');
-// }
-
-// void transferpp(){
-  
-// }
+InputElement inputcontent;
+InputElement submit;
+OutputElement outputcontent;
 
 void main() {
-  InputElement username = querySelector('#username');
-  InputElement submit = querySelector('#save');
-  Element output = querySelector('#username-output');
-  Storage localStorage = window.localStorage;
+  runApp(ng.AppComponentNgFactory);}
 
-  String savedUsername = localStorage['username'];
-  if (savedUsername != null) {
-    output.text = savedUsername;
-  }
-
-  submit.onClick.listen((Event e) {
-    output.text = username.value;
-    localStorage['username'] = username.value;
-  });
+void getstring(){
+  inputcontent = querySelector('#inputcontent');
+  outputcontent=querySelector('#outputcontent');
 }
+
+void transferpp(){
+  
+}
+
+// void main() {
+//   InputElement username = querySelector('#username');
+//   InputElement submit = querySelector('#save');
+//   Element output = querySelector('#username-output');
+//   Storage localStorage = window.localStorage;
+
+//   String savedUsername = localStorage['username'];
+//   if (savedUsername != null) {
+//     output.text = savedUsername;
+//   }
+
+//   submit.onClick.listen((Event e) {
+//     output.text = username.value;
+//     localStorage['username'] = username.value;
+//   });
+// }
   
